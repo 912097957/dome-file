ea23694e22b528262c41867fbf776c56d5e5e6a8
912097957 <912097957@qq.com>
2020-05-06 21:50:37 +0800

    dome file

--- JavaScript/common.js
+++ JavaScript/common.js
@@ -12,5 +12,34 @@
  * @return: 元素对象
  */
 function my$(id) {
-	return document.getElementById(id);
-};
+  return document.getElementById(id);
+}
+
+/* @兼容性
+ * @description: 获取任意标签中的任意文本内容
+ * @param {type} 标签对象
+ * @return: 元素对象的文本内容
+ */
+
+function getInnerText(element) {
+  if (typeof element.textContent == "undefined") { //不支持
+    return element.innerText;
+  } else {
+    return element.textContent;
+  }
+}
+
+/* @兼容性
+ * @description: 设置任意标签中的任意文本内容
+ * @param {type} 标签对象,设置文本内容
+ * @return: 设置后元素对象新的的文本内容
+ */
+
+function setInnerText(element, text) {
+  //判断浏览器是否支持这个属性
+  if (typeof element.textContent == "undefined") { //不支持
+    element.innerText = text;
+  } else {
+    element.textContent = text;
+  }
+}
