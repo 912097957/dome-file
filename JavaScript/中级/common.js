60fb0abbafe9edaa7b135f6505f2096b8303dfba
912097957 <912097957@qq.com>
2020-05-29 21:18:33 +0800

    dome file

--- JavaScript/中级/common.js
+++ JavaScript/中级/common.js
@@ -97,7 +97,7 @@ function getLastElement(element) {
 
 function addEventListener(element, type, fn) {
     //判断浏览器是否支持这个方法
-    if (element.addEventListener()) {
+    if (element.addEventListener) {
         element.addEventListener(type, fn, false);
     } else if (element.attachEvent) {
         element.attachEvent("on" + type, fn);
@@ -170,30 +170,30 @@ function animate(element, json, fn) {
     clearInterval(element.timeId); //清理定时器
     //定时器,返回的是定时器的id
     element.timeId = setInterval(function () {
-        var flag = true; //默认,假设,全部到达目标
+        let flag = true; //默认,假设,全部到达目标
         //遍历json对象中的每个属性还有属性对应的目标值
-        for (var attr in json) {
+        for (let attr in json) {
             //判断这个属性attr中是不是opacity
             if (attr == "opacity") {
                 //获取元素的当前的透明度,当前的透明度放大100倍
-                var current = getStyle(element, attr) * 100;
+                let current = getStyle(element, attr) * 100;
                 //目标的透明度放大100倍
-                var target = json[attr] * 100;
-                var step = (target - current) / 10;
+                let target = json[attr] * 100;
+                let step = (target - current) / 10;
                 step = step > 0 ? Math.ceil(step) : Math.floor(step);
                 current += step; //移动后的值
                 element.style[attr] = current / 100;
-            } else if (attr == "zIndex") { //判断这个属性attr中是不是zIndex
+            } else if (attr == "z-index") { //判断这个属性attr中是不是zIndex
                 //层级改变就是直接改变这个属性的值
                 element.style[attr] = json[attr];
             } else {
                 //普通的属性
                 //获取元素这个属性的当前的值
-                var current = parseInt(getStyle(element, attr));
+                let current = parseInt(getStyle(element, attr));
                 //当前的属性对应的目标值
-                var target = json[attr];
+                let target = json[attr];
                 //移动的步数
-                var step = (target - current) / 10;
+                let step = (target - current) / 10;
                 step = step > 0 ? Math.ceil(step) : Math.floor(step);
                 current += step; //移动后的值
                 element.style[attr] = current + "px";
